31f6ab21f9cdfe9e74d488fce646e8f177649b7c
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-05 17:27:01 +0100

    added component img with framer motion

--- components/Img.js
+++ components/Img.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
-const imgRow = ({ children }) => (
-  <div className="imgContainer">
-    {children}
-    <style jsx>{`
-      .imgContainer {
-        width: 100%;
-        max-width: 25vh;
-        margin: auto;
-      }
-    `}</style>
-  </div>
-);
-export default imgRow;
+const Img = ({ children }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 0.8 }}
+      style={{
+        zIndex: 1,
+        maxWidth: "43vw",
+      }}
+      variants={{
+        visible: { top: 0 },
+        hidden: { top: -10 },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default Img;
