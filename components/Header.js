300f2c54e989a317758562ce6e8d895fd78c1169
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-05 19:49:25 +0100

    rmoved import translateReapeat

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import Picture from "./Picture.js";
-import TranslateRepeat from "./TranslateRepeat.js";
 
 const Header = () => (
   <header>
