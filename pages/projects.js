8ed71737036256fb745542261727298e167131ce
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-17 20:02:08 +0100

    modified ul project and font

--- pages/projects.js
+++ pages/projects.js
@@ -6,19 +6,20 @@ import SEO from "../components/Seo";
 import Picture from "../components/Picture.js";
 import Img from "../components/Img.js";
 import Svg from "../components/Svg.js";
+import Stain from "../components/Stain.js";
+import StainPink from "../components/StainPink.js";
 
 const ProjectsPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="content">
-      <Svg title="il était une fois" category="blog" />
-
-      <Svg title="haute couture" category="pantone" />
-
-      <Svg
-        title="childhood to young adults"
-        category="Psychologue&Psychothérapeute"
-      />
+      <h1>Projects</h1>
+      <div id="span">
+        <span>Il était une fois </span>
+        <span>Pantone et la Haute couture</span>
+        <span>Kiru</span>
+        <span>Children to young Adults</span>
+      </div>
     </div>
     <style jsx>{`
       .content {
@@ -26,7 +27,15 @@ const ProjectsPage = () => (
         grid-template-columns: repeat(2, minmax(0, 1fr));
         align-self: auto;
         justify-self: center;
-        margin: 2rem;
+      }
+      #span {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+      }
+      #span span {
+        font-size: 2rem;
+        padding: 1rem;
       }
     `}</style>
   </Layout>
