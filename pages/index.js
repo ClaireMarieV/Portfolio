e1bdc26db4e5078cef80e159294c92a5b312d519
Claire Vaney <clairevan.54.cv@gmail.com>
2021-01-19 09:47:44 +0100

    chzngerd design index pages

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,11 @@ import TranslateLeft from "../components/TranslateLeft.js";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="content">
-      <div className="name">
-        <h1>ABOUT</h1>
-        <h1>_ME</h1>
+    <div className="content-grid">
+      <div id="name">
+        <h2>Hello, i'm claire marie Vaney</h2>
+        <h2>Freelance front- end developer </h2>
+        <h2>and UX/UI designer</h2>
       </div>
 
       <Img>
@@ -25,19 +26,16 @@ const IndexPage = () => (
       <h3>What I like</h3>
       <TranslateRight id="translate-right">
         <p>
-          Few years ago, i did Mise à Niveau en Arts Appliqués. This year taught
-          me the designer's methodology and the different processes. More than
-          two years ago, i'm retraining in web development after learned Java
-          alone. After traineeship where i worked with Symfony and first job in
-          web development Fullstack, i learned React independently .
+          For release me, I pratice sport (dance, workout or boxe). For several
+          years, I love design. I like the design whether it is fashion,
+          product, space or web. This is why I love web development so much.
         </p>
+        <h2>I choose front-development and UX/UI.</h2>
+        <h2>Mix the both is to mix logic and creation.</h2>
       </TranslateRight>
 
       <h3>Why I became a developer</h3>
       <TranslateRight id="translate-right">
-        <h2>I choose front-development and UX/UI.</h2>
-        <h2>Mix the both is to mix logic and creation.</h2>
-
         <p>
           Few years ago, i did Mise à Niveau en Arts Appliqués. This year taught
           me the designer's methodology and the different processes. More than
@@ -46,47 +44,43 @@ const IndexPage = () => (
           web development Fullstack, i learned React independently .
         </p>
       </TranslateRight>
+      <h3 id="skills">My main advantages</h3>
 
-      <div id="skills">
-        <h3>My main advantages</h3>
-      </div>
-      <button>
-        <span>Download my CV</span>
-      </button>
-      <div className="skills">
-        <TranslateRight>
-          <Icon picture="svg/computerBis.svg" title="WEB DEVELOPMENT" />
-          <Icon picture="svg/responsiveBis.svg" title="RESPONSIVE" />
-        </TranslateRight>
-        <TranslateRight>
-          <Icon picture="svg/UX.svg" title="UX/UI" />
-        </TranslateRight>
-      </div>
+      <button download="CV">Download my CV</button>
     </div>
 
-    <div className="skills"></div>
-
+    <TranslateLeft>
+      <Icon picture="svg/computerBis.svg" title="WEB DEVELOPMENT" />
+      <Icon picture="svg/responsiveBis.svg" title="RESPONSIVE" />
+      <Icon picture="svg/UX.svg" title="UX/UI" />
+    </TranslateLeft>
     <style jsx>{`
-      .content {
+      .content-grid {
         display: grid;
         grid-template-columns: repeat(2, minmax(0, 1fr));
         grid-template-rows: auto;
+        grid-gap: 3rem;
         margin: 2rem;
       }
-      .content h3 {
+      .content-grid h3 {
         display: flex;
         justify-content: center;
         align-items: center;
       }
-      .name {
+
+      #name h2 {
+        font-size: 2rem;
+      }
+      #name {
         justify-self: center;
+        align-self: center;
       }
       .img {
         max-width: 50vh;
       }
 
       .face {
-        box-shadow: -1px -1px 18px 2px rgba(0, 0, 0, 0.2);
+        box-shadow: -1px -1px 18px 2px rgba(0, 0, 0, 0.1);
       }
 
       .arrow {
@@ -96,8 +90,30 @@ const IndexPage = () => (
         display: flex;
         justify-content: space-evenly;
       }
-      .skills {
-        grid-column: 2/3;
+      .content-grid h2 {
+        letter-spacing: 0.05em;
+        transform: translate3d(-0.5em, 0, 0);
+        transition: transform 0.5s;
+        transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
+      }
+      .content-grid h2::before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        width: 55%;
+        height: 0.5rem;
+        left: 0.05em;
+        top: 1.25em;
+        background: #f6e056;
+        transform: scale3d(1, 1, 1);
+        transform-origin: 100% 50%;
+        transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
+      }
+
+      @media (max-width: 700px) {
+        .content-grid {
+          grid-template-columns: auto;
+        }
       }
     `}</style>
   </Layout>
