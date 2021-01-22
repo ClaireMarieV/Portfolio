c7ba6f852243acba8c8e197169c5a71c38635434
Claire Vaney <clairevan.54.cv@gmail.com>
2021-01-22 15:15:05 +0100

    removed css white line before

--- components/Fonts.js
+++ components/Fonts.js
@@ -10,12 +10,6 @@ const Icon = ({ font, letters, sentence, className, id }) => (
     <p>{letters}</p>
 
     <style jsx>{`
-      #font-content p {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-      }
       #font {
         display: grid;
         grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -35,25 +29,6 @@ const Icon = ({ font, letters, sentence, className, id }) => (
         justify-self: center;
         font-size: 2rem;
         font-weight: 600;
-      }
-      #font-content p {
-        letter-spacing: 0.05em;
-        transform: translate3d(-0.5em, 0, 0);
-        transition: transform 0.5s;
-        transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
-      }
-      #font-content p::before {
-        content: "";
-        position: absolute;
-        z-index: -1;
-        width: 55%;
-        height: 0.5rem;
-        left: 0.05em;
-        top: 2.25em;
-        background: #f6e056;
-        transform: scale3d(1, 1, 1);
-        transform-origin: 100% 50%;
-        transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
       }
     `}</style>
   </div>
