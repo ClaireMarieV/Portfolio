900a14c688ed693e884f17e9a1c6fe7c4a79fa1e
Claire Vaney <clairevan.54.cv@gmail.com>
2021-01-22 15:12:00 +0100

    added animation svg line

--- components/Svg.js
+++ components/Svg.js
@@ -1,77 +1,38 @@
 import React from "react";
-import Link from "next/link";
-import { fromArticle } from "../lib/link";
 
-const Svg = ({ title, category, className, id, a }) => (
-  <div id="wrapper">
-    <svg id="stain" viewBox="0 0 1440 800" xmlns="http://www.codrops.com/">
-      <path
-        fill="#E5E1E2"
-        id="base"
-        d="M 1066,436 C 1051,543.8 973.2,656.2 873.6,700.1 756.6,751.7 600.9,
-              725 492.4,657.4 431.5,619.5 387.5,546.9 376.7,476 360.3,368.3 377.9,
-              229.2 462.5,160.5 589.5,57.34 815.4,42.24 952.4,131.7 1044,190.8 1081,328.8 1066,436 Z;"
-      >
+const Svg = () => (
+  <div className="line">
+    <svg id="line" viewBox="0 0 300 50" xmlns="http://www.w3.org/1999/xlink">
+      <path d="M13.6,21.9c0-0.7,352.1,1.2,354,0">
         <animate
           attributeName="d"
-          repeatCount="indefinite"
-          dur="4s"
-          values="M 1041,450.4 C 1023,547.7 992.8,667.7 905.7,714.5 793.1,
-                775 639,728.7 524.5,671.8 453.3,636.4 382.2,575.4 360.2,499 329.7,
-                393 344.6,249.2 426,174.9 568.6,44.66 851.1,-8.71 1002,111.8 1091,182.7 1061,
-                338.6 1041,450.4 Z;M 1066,436 C 1051,543.8 976.5,664.5 873.6,700.1 761,739.1 636.4,
-                655.8 529.5,603.1 441.6,559.8 325.8,520.1 293.8,427.5 263.1,338.4 294.5,213.4 368.2,
-                154.8 520.7,33.48 790.1,23.76 952.4,131.7 1043,191.7 1081,328.8 1066,436 Z;M 1066,436 C 1053
-                531.1 930.7,580.1 842.2,617.2 734,662.7 598.4,707.8 492.4,657.4 427.6,626.6 387.5,546.9 376.7,
-                476 360.3,368.3 376.9,227.9 462.5,160.5 567.6,77.69 749.9,37.5 863.8,148.8 947.6,230.7 1082,320.1 1066,436 Z;
-
-
-                M 1066,436 C 1051,543.8 973.2,656.2 873.6,700.1 756.6,751.7 600.9,
-                725 492.4,657.4 431.5,619.5 387.5,546.9 376.7,476 360.3,368.3 377.9,
-                229.2 462.5,160.5 589.5,57.34 815.4,42.24 952.4,131.7 1044,190.8 1081,
-                328.8 1066,436 Z"
+          dur="1s"
+          begin="mouseover"
+          values="M21.1,87.8c0-0.7,170.5-170.7,353.4-2.8;
+              M13.5,55.9c0-0.7,167.5-80.9,353.4-2.8;
+              M13.6,21.9c0-0.7,352.1,1.2,354,0;
+              M13.5,55.9c0-0.7,167.5-80.9,353.4-2.8;
+              M12.3,55.9c0-0.7,101.5-80.9,353.6-2.8;
+              M13.6,21.9c0-0.7,352.1,1.2,354,0"
         />
       </path>
+      <style jsx>{`
+        .line {
+          z-index: 1
+          display:flex;
+        }
+        svg {
+          min-width: fit-content;
+        }
+        path {
+          fill: none;
+          height: 5rem;
+          stroke: #1d201f;
+          stroke-width: 0.5;
+          transition: all 0.35s easeOut;
+        }
+      `}</style>
     </svg>
-    <Link href={fromArticle({ category, title })}>
-      <a>
-        <div className="title">
-          <h2>{title}</h2>
-          <h4>{category}</h4>
-        </div>
-      </a>
-    </Link>
-    <style jsx>{`
-      a {
-        font-size: 4.5rem;
-        padding: 1rem;
-        color: #121212;
-        border: 3px solid #ededed;
-        border-radius: 10px;
-      }
-      #wrapper {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-      }
-      a {
-        position: absolute;
-      }
-      .title {
-        z-index: 2;
-      }
-      .title h3 {
-        font-weight: bold;
-      }
-      svg {
-        z-index: -1;
-        width: 60vw;
-      }
-      path {
-        transition: 0.4s;
-      }
-    `}</style>
   </div>
 );
 
--- data/2020/blog.js
+++ data/2020/blog.js
@@ -1,45 +1,64 @@
-import Picture from "../../components/picture";
+import Picture from "../../components/Picture";
+import Colors from "../../components/Colors";
+import Fonts from "../../components/Fonts";
+import Svg from "../../components/Svg";
+import Content from "../../components/Content";
+import ContentFlex from "../../components/ContentFlex";
 
-import Home from "../../public/pictures/HomeBlog.jpg?resize&sizes[]=250&sizes[]=400";
-import HomePreview from "../../public/pictures/HomeBlog.jpg?lqip";
-
+import pink from "../../public/svg/colors/colorBlogPink.svg";
+import grey from "../../public/svg/colors/colorBlogGrey.svg";
+import dark from "../../public/svg/colors/colorBlog.svg";
+import gravesend from "../../public/svg/fonts/gravesend.svg";
+import arboria from "../../public/svg/fonts/arboria.svg";
+import raleway from "../../public/svg/fonts/raleway.svg";
 export default {
   title: "Il etait une fois",
 
   content: (
     <>
-      <div>
-        <Picture
-          picture={{ preview: HomePreview, picture: Home }}
-          alt="Index blog"
-        />
-        <h3>Colors</h3>
-        <div>
-          <img
-            src="../../public/svg/colors/colorBlogPink.svg"
-            alt="Couleur beige rosé"
+      <Content>
+        <h3>colors</h3>
+        <div className="colors">
+          <div id="line"></div>
+          <Colors
+            picture={{ picture: pink }}
+            colorHex="#ecbf9e"
+            alt="couleur beige rosé"
           />
-          <img
-            src="../../public/svg/colors/colorBlogPink.svg"
-            alt="Couleur gris rosé"
+          <Colors
+            picture={{ picture: grey }}
+            colorHex="#ece0d5"
+            alt="couleur gris clair rosé"
           />
-          <img
-            src="../../public/svg/colors/colorBlogPink.svg"
-            alt="Couleur gris foncé opaque"
+          <Colors
+            picture={{ picture: dark }}
+            colorHex="#262525"
+            alt="couleur gris foncé opaque"
           />
         </div>
-      </div>
-      <h3>Fonts</h3>
+      </Content>
+      <Content>
+        <h3>fonts</h3>
+        <Fonts
+          font="GRAVESEND, SANS SERIF"
+          letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+          sentence="Let's go to create something"
+        />
+        <Svg />
+        <Fonts
+          font="ARBORIA, SANS-SERIF"
+          letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+          sentence="Let's go to create something"
+        />
+        <Svg />
 
-      <div>
-        <img src="" alt="Couleur beige rosé" />
-        <img src="" alt="Couleur gris rosé" />
-        <img src="" alt="Couleur gris foncé opaque" />
-      </div>
-
-      <p></p>
-
-      <p></p>
+        <Fonts
+          font="RALEWAY, SANS-SERIF"
+          letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+          sentence="Let's go to create something"
+        />
+        <Svg />
+      </Content>
     </>
   ),
 };
