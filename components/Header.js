0b8815e8ce2aa278f6ce2b845707d8dde96b5a85
ClaireMarieV <>
2021-02-15 22:58:19 +0100

    removed code css unused

--- components/Header.js
+++ components/Header.js
@@ -57,22 +57,6 @@ const Header = () => (
         display: inline-flex;
         padding: 15px 20px;
       }
-      .ul > a > span:after {
-        background: none repeat scroll 0 0 transparent;
-        bottom: 0;
-        content: "";
-        display: block;
-        height: 2px;
-        left: 50%;
-        position: absolute;
-        background: white;
-        transition: width 0.3s ease 0s, left 0.3s ease 0s;
-        width: 0;
-      }
-      .ul a:hover:after {
-        width: 100%;
-        left: 0;
-      }
 
       @media screen and (max-height: 300px) {
         ul {
