e3b1f4586adbf6c26e698dcd15042d361dce02c7
ClaireMarieV <>
2021-02-15 20:02:48 +0100

    added curve line and delete svg unused

--- components/Svg.js
+++ components/Svg.js
@@ -2,37 +2,43 @@ import React from "react";
 
 const Svg = () => (
   <div className="line">
-    <svg id="line" viewBox="0 0 300 50" xmlns="http://www.w3.org/1999/xlink">
-      <path d="M13.6,21.9c0-0.7,352.1,1.2,354,0">
+    <svg
+      width="1000"
+      height="100"
+      viewBox="0 300 150"
+      xmlns="http://www.w3.org/1999/xlink"
+    >
+      <path d="M8.2,52.3c0-0.1,854,0,854,0">
         <animate
           attributeName="d"
-          dur="1s"
+          dur="1.1s"
           begin="mouseover"
-          values="M21.1,87.8c0-0.7,170.5-170.7,353.4-2.8;
-              M13.5,55.9c0-0.7,167.5-80.9,353.4-2.8;
-              M13.6,21.9c0-0.7,352.1,1.2,354,0;
-              M13.5,55.9c0-0.7,167.5-80.9,353.4-2.8;
-              M12.3,55.9c0-0.7,101.5-80.9,353.6-2.8;
-              M13.6,21.9c0-0.7,352.1,1.2,354,0"
+          values="M11,54c612.7-10.1,558.6-101.6,847,0.6;
+            M8,80.5c279.6-4.7,338.8-167.5,847.6,0;
+            M5.5,54.5c435.6-4,152.9-106.4,855.8,0.6;
+            M6.2,83C403-75,513,83,853.8,83;
+            M17.5,65.4c288.4-102.2,234.3-10.7,847-0.6;
+            M8.2,52.3c0-0.1,854,0,854,0;"
         />
       </path>
-      <style jsx>{`
-        .line {
-          z-index: 1
-          display:flex;
-        }
-        svg {
-          min-width: fit-content;
-        }
-        path {
-          fill: none;
-          height: 5rem;
-          stroke: #EFF2F5;
-          stroke-width: 2;
-          transition: all 0.35s easeOut;
-        }
-      `}</style>
     </svg>
+    <style jsx>{`
+      .line {
+        display: flex;
+        justify-content: center;
+        justify-self: center;
+      }
+      svg {
+        min-width: fit-content;
+      }
+      path {
+        fill: none;
+        height: 5rem;
+        stroke: #eff2f5;
+        stroke-width: 0.3rem;
+        transition: all 0.1s easeOut;
+      }
+    `}</style>
   </div>
 );
 
