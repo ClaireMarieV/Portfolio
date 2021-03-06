7c9a0fe956d55cb5ad7425d64a632c255b344ce4
ClaireMarieV <>
2021-03-06 16:09:29 +0100

    removed old blob page

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,7 @@
-import blob from "./2020/blob";
+import organic_blob from "./2020/blob";
 import children_to_young_adults from "./2020/alice";
 
 export default {
-  blob,
+  organic_blob,
   children_to_young_adults,
 };
