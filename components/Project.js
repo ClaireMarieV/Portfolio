b3627bb8994e17b88b7c79397a8789ff9ebc2310
ClaireMarieV <>
2021-03-09 20:20:08 +0100

    removed css

--- components/Project.js
+++ components/Project.js
@@ -4,32 +4,17 @@ const Project = ({ children, img, category, title, className, id }) => (
   <>
     <div className={"project" + (className || "")} id={id}>
       <div className="title-project">
-        <h1>{title}</h1>
-        <span>{category}</span>
+        <h2>{title}</h2>
+        <h3>{category}</h3>
       </div>
 
       <div>{children}</div>
 
       <style jsx global>{`
-        .project {
-          display: grid;
-          grid-template-columns: repeat(1, minmax(0, 1fr));
-          grid-template-rows: auto;
-          margin: 2rem;
-        }
-        .project h1 {
-          font-size: 4rem;
-          text-transform: uppercase;
-        }
         .title-project {
           display: flex;
           flex-direction: column;
-          gap: 2rem;
           align-items: baseline;
-        }
-
-        img.logo {
-          width: 25rem;
         }
       `}</style>
     </div>
