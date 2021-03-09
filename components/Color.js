0f3ecd56cabd0e513362a7c23c095385136a9c69
ClaireMarieV <>
2021-03-09 19:09:42 +0100

    added visibility code hexa hover

--- components/Color.js
+++ components/Color.js
@@ -3,21 +3,36 @@ import React from "react";
 const Colors = ({ code }) => (
   <div>
     <div className="color" style={{ backgroundColor: code }}></div>
-    <div>{code}</div>
+    <div className="code">
+      <span>{code}</span>
+    </div>
     <style jsx>{`
       display: flex;
       flex-direction: column;
       flex-grow: 1;
       width: 100%;
       align-items: center;
-    }
+      position: relative;
+      justify-content: center;
+
       .color {
         min-height: 3rem;
       }
-      .color:nth-child(2) {
-        align-sef: center;
-
+      .code {
+        align-self: center;
         justify-content: center;
+        position: absolute;
+        visibility: hidden;
+        height: 100%;
+      }
+      .code:hover,
+      .color:hover ~ .code {
+        visibility: visible;
+      }
+      .code span {
+        font-weight: 600;
+        color: #494949;
+        font-size: 1.2rem;
       }
     `}</style>
   </div>
