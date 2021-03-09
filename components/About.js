d923c3a1f471aa594df8d42eac8645fd82d9e511
ClaireMarieV <>
2021-03-09 19:06:07 +0100

    added width

--- components/About.js
+++ components/About.js
@@ -1,7 +1,12 @@
 const About = ({ project }) => (
-  <div>
+  <div className="about">
     <h4>About the project</h4>
     <p>{project.about}</p>
+    <style jsx>{`
+      .about {
+        width: 35vw;
+      }
+    `}</style>
   </div>
 );
 
