13e4b35d8c0ad3452686425a1f934d5c0130d938
ClaireMarieV <>
2021-03-16 21:50:07 +0100

    finished effect hover view project

--- components/ArticleProject.js
+++ components/ArticleProject.js
@@ -24,6 +24,7 @@ const ArticleProject = ({ title, category, img, imgHover, className }) => (
               viewBox="0 0 210 120"
             >
               <path
+                stroke-dasharray="172.4788055419922"
                 d="M106.9,11.7c0.8-1.9-1.9-3.8-5.5-4.7c-3.6-0.9-7.9-1-12-1C58.1,6.4,29.7,15.1,9.1,26.1c-3,1.6-6.1,3.6-4.8,5.7
           c1.5,2.4,8,3.2,13.4,3.3c21.2,0.3,41.6-3.4,61.7-6.7c4.1-0.7,8.4-1.3,12.7-1.1"
               />
@@ -42,37 +43,33 @@ const ArticleProject = ({ title, category, img, imgHover, className }) => (
         width: 100%;
       }
 
-      path {
-        fill: none;
-        stroke: #000000;
-        stroke-width: 0.7;
-        stroke-miterlimit: 10;
-      }
-      link-view:hover .line__graphic {
-        stroke-dashoffset: 1;
-        transition-timing-function: cubic-bezier(0.8, 1, 0.7, 1);
-        transition-duration: 0.3s;
-      }
       .link-view {
         position: relative;
       }
       .link-view svg {
-        width: 150%;
+        width: 100%;
         position: absolute;
-        transform: translateX(-70%) translateY(-17%);
-        left: 100%;
-        top: 50%;
         z-index: -1;
-        visibility: hidden;
       }
-      .link-view:hover svg {
+      .link-view path {
+        fill: none;
+        stroke: #000000;
+        stroke-width: 0.7;
+        stroke-dasharray: 172.49;
+        stroke-dashoffset: 172.49;
+        visibility: hidden;
+        transition: stroke-dashoffset 0.4s cubic-bezier(0.7, 0, 0.3, 1);
+      }
+      .link-view:hover path {
         visibility: visible;
-        stroke-dasharray: 200;
+        stroke-dasharray: 172.49;
+        stroke-dashoffset: 0;
+        transition-timing-function: cubic-bezier(0.8, 1, 0.7, 1);
+        transition-duration: 0.5s;
       }
       .title {
         padding: 1rem;
       }
-
       .title h2 {
         margin: 0;
         text-transform: uppercase;
