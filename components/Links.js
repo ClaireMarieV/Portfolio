c72a89fe9928ff0b367a2406ce6ca9d959f5c5b5
ClaireMarieV <>
2021-03-17 10:17:21 +0100

    added hover svg contour

--- components/Links.js
+++ components/Links.js
@@ -1,55 +1,85 @@
 const Links = ({ project }) => (
   <div className="link-view">
-    {project.links.source && (
-      <a href={project.links.source} target="_blank">
-        <h3>View source code</h3>
-      </a>
-    )}
+    <div className="link">
+      {project.links.source && (
+        <a href={project.links.source} target="_blank">
+          <svg
+            className="line__graphic"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="150px"
+            y="75px"
+            viewBox="0 0 210 120"
+          >
+            <path
+              d="M106.9,11.7c0.8-1.9-1.9-3.8-5.5-4.7c-3.6-0.9-7.9-1-12-1C58.1,6.4,29.7,15.1,9.1,26.1c-3,1.6-6.1,3.6-4.8,5.7
+        c1.5,2.4,8,3.2,13.4,3.3c21.2,0.3,41.6-3.4,61.7-6.7c4.1-0.7,8.4-1.3,12.7-1.1"
+            />
+          </svg>
+          <h3>View source code</h3>
+        </a>
+      )}
+    </div>
+    <div className="link">
+      {project.links.website && (
+        <a href={project.links.website} target="_blank">
+          <h3>View website</h3>
+          <svg
+            className="line__graphic"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="150px"
+            y="75px"
+            viewBox="0 0 210 120"
+          >
+            <path
+              d="M106.9,11.7c0.8-1.9-1.9-3.8-5.5-4.7c-3.6-0.9-7.9-1-12-1C58.1,6.4,29.7,15.1,9.1,26.1c-3,1.6-6.1,3.6-4.8,5.7
+        	c1.5,2.4,8,3.2,13.4,3.3c21.2,0.3,41.6-3.4,61.7-6.7c4.1-0.7,8.4-1.3,12.7-1.1"
+            />
+          </svg>
+        </a>
+      )}
+    </div>
 
-    {project.links.website && (
-      <a href={project.links.website} target="_blank">
-        <h3>View website</h3>
-      </a>
-    )}
     <style jsx>{`
       .link-view {
         display: grid;
         grid-template-columns: 1fr 1fr;
+        align-items: center;
+        justify-items: center;
       }
-      .link-view a {
-        justify-self: center;
-        align-self: center;
-        transform: translate(0px, 0px);
-        transform-origin: 100% 50%;
-        margin: 1rem;
-      }
+
       .link-view a h3 {
         line-height: 1.4;
-        transition: all 1s;
         transition: all 0.5s cubic-bezier(0.77, 0, 0.175, 1);
         margin: 1rem;
       }
-      .link-view a h3::before {
-        content: "";
+      .link {
+        position: relative;
+      }
+      .link svg {
         position: absolute;
         left: 0;
         top: 0;
-        bottom: 0;
-        margin: auto;
-        width: 50px;
-        height: 50px;
-        background: #dedbeb;
-        border-radius: 50%;
+        width: 200%;
         z-index: -1;
-        transform-origin: 0% 50%;
-        transition: transform 1s;
-        transition: all 0.5s cubic-bezier(0.77, 0, 0.175, 1);
       }
-      .link-view a h3:hover::before,
-      .link-view a h3:focus::before {
-        transform: scale3d(1, 1, 1);
-        width: 15rem;
-        border-radius: 30px;
+
+      path {
+        fill: none;
+        stroke: #000000;
+        stroke-width: 1;
+        stroke-dasharray: 200.49;
+        stroke-dashoffset: 200.49;
+        transition: stroke-dashoffset 0.4s cubic-bezier(0.7, 0, 0.3, 1);
+      }
+      .link:hover path {
+        stroke-dasharray: 200.49;
+        stroke-dashoffset: 0;
+        transition-timing-function: cubic-bezier(0.8, 1, 0.7, 1);
+        transition-duration: 0.5s;
       }
       @media (max-width: 600px) {
         .link-view {
