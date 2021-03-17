d6f4d0c5c0e4740b97836954f313c966c8aa85f5
ClaireMarieV <>
2021-03-17 09:48:55 +0100

    fixed name article blog

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,5 @@
 import identity from "./2020/identity";
-import il_etait_une_fois from "./2020/blog";
+import once_upon_a_time from "./2020/blog";
 import organic_blob from "./2020/blob";
 import children_to_young_adults from "./2020/alice";
 import felicitylab from "./2021/felicity";
@@ -7,7 +7,7 @@ import felicitylab from "./2021/felicity";
 export default {
   organic_blob,
   children_to_young_adults,
-  il_etait_une_fois,
+  once_upon_a_time,
   felicitylab,
   identity,
 };
