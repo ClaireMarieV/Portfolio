c59458a1c455856255d78fd6ef6baac7c4e2cebe
ClaireMarieV <>
2021-03-17 09:37:36 +0100

    changed hover effect svg

--- components/ArticleProject.js
+++ components/ArticleProject.js
@@ -14,22 +14,18 @@ const ArticleProject = ({ title, category, img, imgHover, className }) => (
       <div className="link-view">
         <Link href={fromArticle({ title })}>
           <a>
+            <h3>View project </h3>
             <svg
               className="line__graphic"
               version="1.1"
               id="Layer_1"
               xmlns="http://www.w3.org/2000/svg"
               x="150px"
-              y="75px"
-              viewBox="0 0 210 120"
+              y="35px"
+              viewBox="0 0 350 150"
             >
-              <path
-                stroke-dasharray="172.4788055419922"
-                d="M106.9,11.7c0.8-1.9-1.9-3.8-5.5-4.7c-3.6-0.9-7.9-1-12-1C58.1,6.4,29.7,15.1,9.1,26.1c-3,1.6-6.1,3.6-4.8,5.7
-          c1.5,2.4,8,3.2,13.4,3.3c21.2,0.3,41.6-3.4,61.7-6.7c4.1-0.7,8.4-1.3,12.7-1.1"
-              />
+              <path d="M4.5,5.5c76,8,167-4,176-2" />
             </svg>
-            <h3>View project </h3>
           </a>
         </Link>
       </div>
@@ -54,7 +50,7 @@ const ArticleProject = ({ title, category, img, imgHover, className }) => (
       .link-view path {
         fill: none;
         stroke: #000000;
-        stroke-width: 0.7;
+        stroke-width: 1.5;
         stroke-dasharray: 200.49;
         stroke-dashoffset: 200.49;
         visibility: hidden;
