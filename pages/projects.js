b925b973086f40bc0bed544ca46592df715a28a5
ClaireMarieV <>
2021-03-18 11:08:28 +0100

    refonte page projects

--- pages/projects.js
+++ pages/projects.js
@@ -5,6 +5,7 @@ import SEO from "../components/Seo";
 import ArticleProject from "../components/ArticleProject";
 import Content from "../components/Content";
 import ArticleLogo from "../components/ArticleLogo";
+import Mail from "../components/Mail";
 
 //Articles
 import blog from "../data/2020/blog";
@@ -15,62 +16,73 @@ import identity from "../data/2020/identity";
 
 const ProjectsPage = () => (
   <Layout>
-    <SEO title="Home" />
     <Content className="content">
-      <div className="title">
-        <h2>My Projects</h2>
+      <div className="listing-projects-title">
+        <h1>Selected works</h1>
+        <span>from 2020 to 2021</span>
       </div>
       <div id="projects">
         <ArticleLogo
           className="identity"
           title={identity.title}
           category="Graphic Design"
+          year="2019"
         />
         <ArticleProject
           className="blog"
           title={blog.title}
           category="ReactJs"
+          year="2020"
         />
         <ArticleProject
           className="blob"
           title={blob.title}
           category="ReactJs"
+          year="2020"
         />
         <ArticleProject
           className="felicity"
           title={felicity.title}
           category="Wordpress"
+          year="2021"
         />
         <ArticleProject
           className="alice"
           title={alice.title}
           category="UI/UX Design"
+          year="2020"
         />
       </div>
+      <Mail />
     </Content>
-    <style jsx>{`
+    <style jsx global>{`
       #projects {
         display: grid;
-        grid-template-columns: repeat(4, 1fr);
+        grid-template-columns: 1fr;
+        grid-template-rows: auto;
         gap: 3rem;
       }
-      .title {
-        position: absolute;
-        z-index: -1;
-        margin: 0 auto;
-        color: #e6eaf2;
+
+      #projects:nth-child(1n + 2) {
+        align-self: end;
       }
-      .title h2 {
-        font-size: 12rem;
+
+      .listing-projects-title h1 {
+        text-transform: uppercase;
+        font-size: 3rem;
+      }
+      .listing-projects-title {
+        display: flex;
+        flex-direction: column;
       }
       @media (max-width: 850px) {
         #projects {
           grid-template-columns: 1fr 1fr;
         }
-        .title h2 {
-          font-size: 2rem;
+        .listing-projects-title h2 {
+          font-size: 4.5rem;
         }
-        .title {
+        .listing-projects-title {
           position: inherit;
           z-index: 0;
           margin: 0 auto;
@@ -81,10 +93,10 @@ const ProjectsPage = () => (
         #projects {
           grid-template-columns: 1fr;
         }
-        .title h2 {
+        .listing-projects-title h2 {
           font-size: 2rem;
         }
-        .title {
+        .listing-projects-title {
           position: inherit;
           z-index: 0;
           margin: 0 auto;
