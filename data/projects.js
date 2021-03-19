89cb913bc67cd741a17f79019ac9ec695fdc72bb
ClaireMarieV <>
2021-03-19 16:37:53 +0100

    fixed name children

--- data/projects.js
+++ data/projects.js
@@ -1,11 +1,11 @@
 import once_upon_a_time from "./2020/blog";
 import organic_blob from "./2020/blob";
-import children_to_young_adults from "./2020/alice";
+import children_and_young_adults from "./2020/alice";
 import felicitylab from "./2021/felicity";
 
 export default {
   organic_blob,
-  children_to_young_adults,
+  children_and_young_adults,
   once_upon_a_time,
   felicitylab,
 };
