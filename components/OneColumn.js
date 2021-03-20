d4ecb56d5cd700c8cc99f6c580c64f0328f374a4
ClaireMarieV <>
2021-03-20 18:07:38 +0100

    renamed content flex to oneColumn

---
+++ components/OneColumn.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const OneColumn = ({ children }) => (
+  <div className="one-column">
+    {children}
+    <style jsx>{`
+      .one-column {
+        display: grid;
+        grid-template-columns: 1fr;
+        margin: 0 auto 4rem auto;
+        line-height: 3rem;
+        gap: 4rem;
+      }
+    `}</style>
+  </div>
+);
+
+export default OneColumn;
