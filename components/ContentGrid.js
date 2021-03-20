15ac902d3b1ab9ab51ebf74e714a6a504ae18cf3
ClaireMarieV <>
2021-03-20 18:04:45 +0100

    fixed content grid

--- components/ContentGrid.js
+++ components/ContentGrid.js
@@ -6,10 +6,11 @@ const ContentGrid = ({ children }) => (
     <style jsx>{`
       .content-grid {
         display: grid;
-        grid-template-columns: 1fr;
+        grid-template-columns: 1fr 1fr;
         margin: 0 auto 4rem auto;
         line-height: 3rem;
         gap: 4rem;
+        width: 44%;
       }
       @media (max-width: 800px) {
         .content-grid {
